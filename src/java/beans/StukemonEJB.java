80b057f975277df6e564e10a787e4b416890f1b2
xavivg <xavivilardellgras@gmail.com>
2017-05-16 19:28:04 +0200

    First commit! 	features: 		-New feature create trainer 		-EJB main functions done(needs some work) 		-Menu with all options.

---
+++ src/java/beans/StukemonEJB.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import entities.Pokemon;
+import entities.Trainer;
+import static java.lang.System.out;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ *
+ * @author usu26
+ */
+@Stateless
+public class StukemonEJB {
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    public boolean trainerExist(String name) {
+        emf.getCache().evictAll();
+        EntityManager em = emf.createEntityManager();
+        Trainer c = em.find(Trainer.class, name);
+        em.close();
+        return c != null;
+    }
+    //if needed PROVISIONAL
+    public Trainer getTrainer(String name) {
+        emf.getCache().evictAll();
+        EntityManager em = emf.createEntityManager();
+        Trainer c = em.find(Trainer.class, name);
+         em.close();
+        if(c !=null){
+            return c;
+        }
+       return null;
+    }
+
+    public boolean createTrainer(Trainer c) {
+        out.println(c.getName()+"HEEEEEEEEEEEEEEEEEEEEEEY");
+        EntityManager em = emf.createEntityManager();
+        if (!trainerExist(c.getName())) {
+            em.persist(c);
+            em.flush();
+            em.close();
+            return true;
+        }
+        return false;
+    }
+    public boolean deleteTrainer(String nombre) {
+        emf.getCache().evictAll();
+        EntityManager em = emf.createEntityManager();
+        Trainer trainer = em.find(Trainer.class, nombre);
+        if(trainer != null){
+            em.remove(trainer);
+            em.close();
+            return true;
+        }
+        return false;
+    }
+     public boolean pokeExist(String name) {
+        emf.getCache().evictAll();
+        EntityManager em = emf.createEntityManager();
+        Pokemon c = em.find(Pokemon.class, name);
+        em.close();
+        return c != null;
+    }
+    //if needed PROVISIONAL
+    public Pokemon getPoke(String name) {
+        emf.getCache().evictAll();
+        EntityManager em = emf.createEntityManager();
+        Pokemon c = em.find(Pokemon.class, name);
+         em.close();
+        if(c !=null){
+            return c;
+        }
+       return null;
+    }
+    public boolean deletePoke(String nombre) {
+        emf.getCache().evictAll();
+        EntityManager em = emf.createEntityManager();
+        Pokemon poke = em.find(Pokemon.class, nombre);
+        if(poke != null){
+            em.remove(poke);
+            em.close();
+            return true;
+        }
+        return false;
+    }
+}
